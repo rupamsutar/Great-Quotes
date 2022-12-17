2c79645d81dbd251e2e61da924b135b8b8c601df
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-17 17:33:37 +0530

    Practicing Nested Routes

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { useParams} from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments"
 const QuoteDetails = () => {
     const params = useParams();
     return(
         <React.Fragment>
             <h1>QuoteDetails</h1>
             <h2>{params.quoteID}</h2>
+            <Route path={`/quotes/${params.quoteID}/comments`}>
+                <Comments />
+            </Route>
         </React.Fragment>
     )
 }
